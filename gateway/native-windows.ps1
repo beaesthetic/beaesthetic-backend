82b2f6a3bdf75680575dfe82b91c64d44837ba92
petretiandrea <petretiandrea@gmail.com>
2024-05-22 20:28:14 +0200

    fix: docker build native gateway

---
+++ gateway/native-windows.ps1
@@ -0,0 +1,5 @@
+# Define the path to vcvars64.bat
+$vcvarsPath = "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+
+# Call vcvars64.bat and then run the Gradle command
+cmd.exe /c "call `"$vcvarsPath`" && gradlew :gateway:nativeCompile -Pnative -DskipTests"
