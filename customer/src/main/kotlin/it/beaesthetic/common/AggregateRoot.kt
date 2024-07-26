3bdc7ace7d2aa24b2cde2d7ea4b64d7620f7b96a
petretiandrea <petretiandrea@gmail.com>
2024-07-26 16:19:06 +0200

    feat(customer): add naive outbox

---
+++ customer/src/main/kotlin/it/beaesthetic/common/AggregateRoot.kt
@@ -0,0 +1,21 @@
+package it.beaesthetic.common
+
+interface DomainEventRegistry<E> {
+    fun addEvent(event: E)
+    fun clearEvents()
+    val events: List<E>
+}
+
+class DomainEventRegistryDelegate<E>(private var domainEvents: List<E> = emptyList()) :
+    DomainEventRegistry<E> {
+    override fun addEvent(event: E) {
+        domainEvents += event
+    }
+
+    override fun clearEvents() {
+        domainEvents = emptyList()
+    }
+
+    override val events: List<E>
+        get() = domainEvents
+}
---
+++ customer/src/main/kotlin/it/beaesthetic/customer/domain/CustomerEvent.kt
@@ -0,0 +1,17 @@
+package it.beaesthetic.customer.domain
+
+sealed interface CustomerEvent
+
+data class CustomerCreated(
+    val customerId: CustomerId,
+    val name: String,
+    val surname: String?,
+    val contacts: Contacts
+) : CustomerEvent
+
+data class CustomerChanged(
+    val customerId: CustomerId,
+    val name: String,
+    val surname: String?,
+    val contacts: Contacts
+) : CustomerEvent
