b6374602e7c0e69f54e6b3d860f961e4f29f7796
petretiandrea <petretiandrea@gmail.com>
2024-08-06 18:15:19 +0200

    fix(notification): use proper api key name

--- notification/src/main/kotlin/it/beaesthetic/notification/DependencyConfiguration.kt
+++ notification/src/main/kotlin/it/beaesthetic/notification/DependencyConfiguration.kt
@@ -8,14 +8,10 @@ import it.beaesthetic.notification.infra.providers.SmsNotificationProvider
 import jakarta.enterprise.context.Dependent
 import jakarta.enterprise.inject.Produces
 import jakarta.inject.Singleton
-import jakarta.ws.rs.core.Response
-import jakarta.ws.rs.ext.ContextResolver
-import jakarta.ws.rs.ext.Provider
 import org.bson.codecs.configuration.CodecProvider
 import org.bson.codecs.pojo.ClassModel
 import org.bson.codecs.pojo.PojoCodecProvider
 import org.eclipse.microprofile.rest.client.inject.RestClient
-import org.jboss.resteasy.reactive.client.handlers.RedirectHandler
 
 @Dependent
 class DependencyConfiguration {
