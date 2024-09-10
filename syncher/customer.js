3ed30b58ef76915a09f878f8338eed773ef05487
petretiandrea <petretiandrea@gmail.com>
2024-09-10 20:30:48 +0200

    syncher add agenda synch

--- syncher/customer.js
+++ syncher/customer.js
@@ -18,31 +18,31 @@ function transformDocument(doc) {
 }
 
 // Connection URL
-const mongoConnectionString = MONGO_CONNECTION_STRING;
+const mongoConnectionString = "mongodb://root:t2yStjNsYLUWe3tF@localhost:27018/?authSource=admin&directConnection=true"; //MONGO_CONNECTION_STRING;
 
 // Database and collection names
 const dbName1 = 'customers';
 const dbName2 = 'customer-v2'
 
-migration(
-    mongoConnectionString,
-    dbName1,
-    dbName2,
-    "customers",
-    "customers",
-    false,
-    transformDocument
-).then(r => console.log("Completed Customer"));
-
-migration(
-    mongoConnectionString,
-    dbName1,
-    dbName2,
-    "fidelitycards",
-    "fidelitycards",
-    false,
-    transformFidelity
-).then(r => console.log("Completed fidelitycards"));
+// migration(
+//     mongoConnectionString,
+//     dbName1,
+//     dbName2,
+//     "customers",
+//     "customers",
+//     false,
+//     transformDocument
+// ).then(r => console.log("Completed Customer"));
+//
+// migration(
+//     mongoConnectionString,
+//     dbName1,
+//     dbName2,
+//     "fidelitycards",
+//     "fidelitycards",
+//     false,
+//     transformFidelity
+// ).then(r => console.log("Completed fidelitycards"));
 
 migration(
     mongoConnectionString,
@@ -65,12 +65,56 @@ function transformFidelity(doc) {
             _type: v._type,
             treatment: v.treatment,
             isUsed: v.isUsed,
-            createdAt: v.createdAt
+            createdAt: v.createdAt ?? new Date(),
         })),
         createdAt: doc.createdAt,
         updatedAt: doc.updatedAt ?? new Date(),
     };
 }
+
+/*
+{
+  "_id": "b73f081a-950f-4b2e-8fc7-756566acda4d",
+  "activeGiftCards": [
+    {
+      "_id": "3a6deb68-3dca-4516-881e-ecb22a10253b",
+      "amount": 0,
+      "amountSpent": 0,
+      "availableAmount": 10,
+      "createdAt": {
+        "$date": "2024-07-26T09:56:59.743Z"
+      },
+      "customerId": "8ee3a426-02d6-4631-95ef-779edf303d52",
+      "expireAt": {
+        "$date": "2025-07-26T09:56:59.743Z"
+      }
+    }
+  ],
+  "availableAmount": 10,
+  "createdAt": {
+    "$date": "2024-07-26T09:56:59.743Z"
+  },
+  "operations": [
+    {
+      "type": "GiftCardMoneyCredited",
+      "amount": 10,
+      "at": {
+        "$date": "2024-07-26T09:56:59.743Z"
+      },
+      "expireAt": {
+        "$date": "2025-07-26T09:56:59.743Z"
+      },
+      "giftCardId": "3a6deb68-3dca-4516-881e-ecb22a10253b"
+    }
+  ],
+  "owner": "8ee3a426-02d6-4631-95ef-779edf303d52",
+  "spentAmount": 0,
+  "updatedAt": {
+    "$date": "2024-07-26T09:56:59.743Z"
+  }
+}
+ */
+
 
 function transformWallet(doc) {
     return {
@@ -78,12 +122,12 @@ function transformWallet(doc) {
         owner: doc.owner,
         activeGiftCards: doc.activeGiftCards.map(g => ({
             _id: g.id,
-            customerId: g.customerId,
-            availableAmount: g.availableAmount,
             amount: g.amount,
             amountSpent: g.amountSpent,
-            expireAt: g.expireAt,
+            availableAmount: g.availableAmount,
             createdAt: g.createdAt,
+            customerId: g.customerId,
+            expireAt: g.expireAt,
         })),
         availableAmount: doc.availableAmount,
         spentAmount: doc.spentAmount,
