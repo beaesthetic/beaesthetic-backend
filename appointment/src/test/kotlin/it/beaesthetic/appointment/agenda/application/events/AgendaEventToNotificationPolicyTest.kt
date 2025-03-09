e4f71319fc3b6facd8e27baa84a352f178d1fb82
petretiandrea <petretiandrea@gmail.com>
2025-03-09 13:32:41 +0100

    fix spotless

--- appointment/src/test/kotlin/it/beaesthetic/appointment/agenda/application/events/AgendaEventToNotificationPolicyTest.kt
+++ appointment/src/test/kotlin/it/beaesthetic/appointment/agenda/application/events/AgendaEventToNotificationPolicyTest.kt
@@ -2,7 +2,6 @@ package it.beaesthetic.appointment.agenda.application.events
 
 import io.smallrye.mutiny.coroutines.awaitSuspending
 import it.beaesthetic.appointment.agenda.application.notification.SendNotificationHandler
-import it.beaesthetic.appointment.agenda.config.NotificationConfiguration
 import it.beaesthetic.appointment.agenda.domain.event.AgendaEvent
 import it.beaesthetic.appointment.agenda.domain.event.AgendaEventScheduled
 import it.beaesthetic.appointment.agenda.domain.event.Attendee
@@ -13,19 +12,13 @@ import org.mockito.kotlin.mock
 
 class AgendaEventToNotificationPolicyTest {
 
-    private val notificationConfiguration =
-        object : NotificationConfiguration {
-            override fun whitelist(): List<String> = listOf("123")
-        }
-
     private lateinit var sendNotificationHandler: SendNotificationHandler
     private lateinit var agentEventToNotificationPolicy: AgendaEventToNotificationPolicy
 
     @BeforeEach
     fun setup() {
         sendNotificationHandler = mock()
-        agentEventToNotificationPolicy =
-            AgendaEventToNotificationPolicy(notificationConfiguration, sendNotificationHandler)
+        agentEventToNotificationPolicy = AgendaEventToNotificationPolicy(sendNotificationHandler)
     }
 
     @Test
