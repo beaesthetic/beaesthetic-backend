35643d17eed1d3b9b6faa0b093f39450d8862946
petretiandrea <petretiandrea@gmail.com>
2025-05-13 08:31:42 +0200

    fix: add customer pagination

--- customer/src/main/kotlin/it/beaesthetic/customer/infra/MongoPageBuilder.kt
+++ customer/src/main/kotlin/it/beaesthetic/customer/infra/MongoPageBuilder.kt
@@ -1,17 +1,57 @@
 package it.beaesthetic.customer.infra
 
+import com.mongodb.client.model.Collation
+import com.mongodb.client.model.CollationStrength
+import io.quarkus.mongodb.FindOptions
+import io.quarkus.mongodb.panache.kotlin.reactive.ReactivePanacheMongoRepository
 import io.quarkus.panache.common.Sort
+import io.smallrye.mutiny.coroutines.awaitSuspending
 import java.util.Base64
 import org.bson.Document
 
-class MongoPageBuilder(
+data class Page<T>(val items: List<T>, val lastItemToken: String?)
+
+class MongoPageBuilder<K, T : Any>(
+    private val repository: ReactivePanacheMongoRepository<T>,
+    private val pageSize: Int,
     private val sortFields: List<String>,
-    private val direction: Sort.Direction
+    private val direction: Sort.Direction,
+    private val idExtractor: (T) -> K
 ) {
 
-    fun generateCursorFilter(pageToken: String) = generateCursorFilter(decodePageToken(pageToken))
+    suspend fun paginate(pageToken: String?): Page<T> {
+        val filter =
+            when {
+                pageToken != null -> generateCursorFilter(pageToken)
+                else -> Document()
+            }
+        var sort = generateSortDocument()
+        var collation =
+            Collation.builder()
+                .locale("en")
+                .caseLevel(false)
+                .collationStrength(CollationStrength.SECONDARY)
+                .build()
+        var items =
+            repository
+                .mongoCollection()
+                .find(FindOptions().filter(filter).sort(sort).limit(pageSize).collation(collation))
+                .collect()
+                .asList()
+                .awaitSuspending()
 
-    fun generateCursorFilter(cursorValues: List<String>): Document {
+        return Page(
+            items,
+            lastItemToken =
+                if (items.size < pageSize) null
+                else getNextPageToken(idExtractor(items.last()), items.last())
+        )
+    }
+
+    private fun generateCursorFilter(pageToken: String) =
+        generateCursorFilter(decodePageToken(pageToken))
+
+    private fun generateCursorFilter(cursorValues: List<String>): Document {
         require(sortFields.size == cursorValues.size) { "Cursor and sort field mismatch" }
         val op = if (direction == Sort.Direction.Ascending) "\$gt" else "\$lt"
 
@@ -28,16 +68,16 @@ class MongoPageBuilder(
         return Document("\$or", orConditions)
     }
 
-    fun generateSortDocument(): Document {
+    private fun generateSortDocument(): Document {
         val sortDir = if (direction == Sort.Direction.Ascending) 1 else -1
         return sortFields.fold(Document()) { doc, field -> doc.append(field, sortDir) }
     }
 
-    fun <T> getNextPageToken(id: String, item: T): String {
+    private fun <T> getNextPageToken(id: K, item: T): String {
         val values =
             sortFields.map { field ->
                 when (field) {
-                    "_id" -> id
+                    "_id" -> id.toString()
                     else ->
                         item!!::class
                             .java
@@ -50,11 +90,11 @@ class MongoPageBuilder(
         return encodePageToken(values)
     }
 
-    fun encodePageToken(values: List<String>): String {
+    private fun encodePageToken(values: List<String>): String {
         return Base64.getEncoder().encodeToString(values.joinToString("__").encodeToByteArray())
     }
 
-    fun decodePageToken(pageToken: String): List<String> {
+    private fun decodePageToken(pageToken: String): List<String> {
         return String(Base64.getDecoder().decode(pageToken)).split("__")
     }
 }
