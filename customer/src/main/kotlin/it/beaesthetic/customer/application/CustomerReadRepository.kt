aa88e3d9527a9d750f86efac930244ae0c0730c8
petretiandrea <petretiandrea@gmail.com>
2025-05-13 07:48:44 +0200

    feat: stable pagination customer

--- customer/src/main/kotlin/it/beaesthetic/customer/application/CustomerReadRepository.kt
+++ customer/src/main/kotlin/it/beaesthetic/customer/application/CustomerReadRepository.kt
@@ -1,9 +1,15 @@
 package it.beaesthetic.customer.application
 
+import io.quarkus.panache.common.Sort
 import it.beaesthetic.customer.domain.Customer
 
 interface CustomerReadRepository {
-    suspend fun findNextPage(pageToken: String?, limit: Int?): CustomerPage
+    suspend fun findNextPage(
+        pageToken: String?,
+        limit: Int?,
+        sortBy: List<String>,
+        sortDirection: Sort.Direction
+    ): CustomerPage
     data class CustomerPage(
         val customers: List<Customer>,
         val pageSize: Int,
