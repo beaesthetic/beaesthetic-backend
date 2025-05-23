8200cda20a2113bf88dca721dc3225f48604d11a
petretiandrea <petretiandrea@gmail.com>
2025-05-23 23:23:52 +0200

    fix: inject named cache

--- customer/src/main/kotlin/it/beaesthetic/common/SuspendableCache.kt
+++ customer/src/main/kotlin/it/beaesthetic/common/SuspendableCache.kt
@@ -1,6 +1,6 @@
 package it.beaesthetic.common
 
-import io.quarkus.cache.CacheManager
+import io.quarkus.cache.Cache
 import io.smallrye.mutiny.coroutines.awaitSuspending
 import io.smallrye.mutiny.coroutines.uni
 import io.vertx.core.Vertx
@@ -11,27 +11,24 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.SupervisorJob
 
 @ApplicationScoped
-class SuspendableCache(private val vertx: Vertx, private val cacheManager: CacheManager) {
+class SuspendableCache(private val vertx: Vertx) {
 
     private val coroutineScope: CoroutineScope by lazy {
         CoroutineScope(vertx.dispatcher() + SupervisorJob())
     }
 
     @OptIn(ExperimentalCoroutinesApi::class)
-    suspend fun <V> getOrLoad(cacheName: String, vararg keys: Any?, loader: suspend () -> V): V {
-        val cache = cacheManager.getCache(cacheName).orElseThrow()
+    suspend fun <V> getOrLoad(cache: Cache, vararg keys: Any?, loader: suspend () -> V): V {
         val cacheKey = keys.filterNotNull().joinToString(":")
         return cache.getAsync(cacheKey) { uni(coroutineScope, loader) }.awaitSuspending()
     }
 
-    suspend fun invalidateAll(cacheName: String) {
-        val cache = cacheManager.getCache(cacheName).orElseThrow()
+    suspend fun invalidateAll(cache: Cache) {
         cache.invalidateAll().awaitSuspending()
     }
 
-    suspend fun invalidateKey(cacheName: String, vararg keys: Any?) {
+    suspend fun invalidateKey(cache: Cache, vararg keys: Any?) {
         val cacheKey = keys.filterNotNull().joinToString(":")
-        val cache = cacheManager.getCache(cacheName).orElseThrow()
         cache.invalidate(cacheKey).awaitSuspending()
     }
 }
