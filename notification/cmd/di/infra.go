0f8a9a8abc74a169726515e8c1c0babb57f6f6a4
petretiandrea <petretiandrea@gmail.com>
2026-07-05 18:42:46 +0200

    fix: ensure file source is imported for database migrations

--- notification/cmd/di/infra.go
+++ notification/cmd/di/infra.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/jackc/pgx/v5/stdlib"
 )
