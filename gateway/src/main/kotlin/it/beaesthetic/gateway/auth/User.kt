75cfd925791eb0d3bded4269e5777d79f17570c7
petretiandrea <petretiandrea@gmail.com>
2026-07-22 19:16:36 +0200

    fix: added customer to release workflow

--- gateway/src/main/kotlin/it/beaesthetic/gateway/auth/User.kt
+++
@@ -1,7 +0,0 @@
-package it.beaesthetic.gateway.auth
-
-data class User(
-    val id: String,
-    val name: String,
-    val email: String
-)
--- gateway/src/main/kotlin/it/beaesthetic/gateway/auth/UserRoles.kt
+++
@@ -1,9 +0,0 @@
-package it.beaesthetic.gateway.auth
-
-typealias Role = String
-
-interface UserRoles {
-    suspend fun findByUserEmail(email: String): Set<Role>
-
-    suspend fun findByUserId(userId: String): Set<Role>
-}
